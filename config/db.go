c4c1a331188e0bdeb86a0cb32bdd35fc4e5f7486
Minh Dao T <minh.dao@asiantech.vn>
2018-03-28 09:56:50 +0700

    change db mysql->postgres

--- config/db.go
+++ config/db.go
@@ -4,15 +4,18 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 //ConnectDB config connect db
 func ConnectDB() *gorm.DB {
 	dbConnect := os.Getenv("DB_CONNECTION")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_DATABASE")
-	db, err := gorm.Open(dbConnect, dbUser+":"+dbPass+"@/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open(dbConnect, "host="+dbHost+" port="+dbPort+" user="+dbUser+" dbname="+dbName+" password="+dbPass)
 	if err != nil {
 		panic(err)
 	}
--- config/env.go
+++ config/env.go
@@ -4,11 +4,11 @@ import "os"
 
 //SetEnv set up env of db
 func SetEnv() {
-	os.Setenv("DB_CONNECTION", "mysql")
-	os.Setenv("DB_HOST", "127.0.0.1")
-	os.Setenv("DB_PORT", "3306")
+	os.Setenv("DB_CONNECTION", "postgres")
+	os.Setenv("DB_HOST", "localhost")
+	os.Setenv("DB_PORT", "5432")
 	os.Setenv("DB_DATABASE", "golang3")
-	os.Setenv("DB_USERNAME", "root")
+	os.Setenv("DB_USERNAME", "postgres")
 	os.Setenv("DB_PASSWORD", "root")
 	os.Setenv("DB_CHARSET", "utf8")
 	os.Setenv("DB_PARSETIME", "True")
